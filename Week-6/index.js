33e6ac3e6cb34ae1411dc047e827926811ed84af
AmanDas2000 <das.aman45@gmail.com>
2022-05-28 17:23:29 +0530

    todo app

--- Week-6/index.js
+++ Week-6/index.js
@@ -143,18 +143,18 @@ const threeSum = (arr, sum) => {
     let curr
     for (let i = 0; i < arr.length; i++){
         let low = i+1;
-        let high = arr.length - 1;
+        let high = arr.length - 1;       
         while (low < high) {
             curr = arr[low] + arr[high] + arr[i];
             
             if (curr === sum) return curr;
-            else if (curr > sum) high--;
-            else if (curr < sum) low++;
-        }
+            else if (curr > sum) { high--;}
+            else if (curr < sum) { low++; }
+        }        
     }
     return curr;
 }
 
-// console.log(threeSum([-1,2 ,1 ,-4],1))
+console.log(threeSum([-1,2,1,-4],1))
 
 
