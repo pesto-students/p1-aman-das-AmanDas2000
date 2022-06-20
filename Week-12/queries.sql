6e82a7a79388a1f5b554804320034958ad89a476
AmanDas2000 <das.aman45@gmail.com>
2022-06-20 19:07:35 +0530

    week 12 assignments

---
+++ Week-12/queries.sql
@@ -0,0 +1,18 @@
+select * from items where itemWeight=(select min(itemWeight) from items);
+
+select * from warehouse where cId=(select cId from city where cityName='Pune');
+
+select * from items where itemNo in (select itemNo from orderItem where oId in (select orders.oId from orders inner join customer where orders.cNo = customer.cNo and cName="Mr. Patil"));
+
+select max(mycount) from (select count(wId)as mycount from store group by wId);
+
+select * from warehouse where wId = (select wId from store group by wId having count(wId)=(select max(mycount) from (select count(wId)as mycount from store group by wId) as maxStore));
+
+
+select * from items where itemNo = (select itemNo from orderItem group by itemNo having count(itemNo) = (select min(mycount) from (select count(itemNo) as mycount, itemNo from orderItem group by itemNo) as minTable));
+
+select * from (select * from customer inner join orders where customer.cNo= orders.cNo) as customerOrders inner join orderItem where customerOrders.oId=orderItem.oId;
+
+select customer.cNo, cName, oId, oDate from customer inner join orders where customer.cNo= orders.cNo;
+
+select orderItem.oId, orders.oDate, orderItem.quantity, customer.cNo, customer.cName, items.itemDescription from (((customer inner join orders on customer.cNo=orders.cNo)inner join orderItem on orders.oId = orderItem.oId) inner join items on orderItem.itemNo = items.itemNo) order by cNo;
