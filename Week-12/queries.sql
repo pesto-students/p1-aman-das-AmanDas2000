71b197d9fe93072e8fff1c2c7e1140848b3b144b
AmanDas2000 <das.aman45@gmail.com>
2022-06-23 15:19:19 +0530

    improvements to SQL queries

--- Week-12/queries.sql
+++ Week-12/queries.sql
@@ -1,13 +1,11 @@
 use pesto;
 
-select * from items where itemWeight=(select min(itemWeight) from items);
+select * from items order by itemWeight asc limit 1;
 
-select * from warehouse where cId=(select cId from city where cityName='Pune');
+select * from warehouse inner join city where city.cId=warehouse.cId and cityName='Pune';
 
-select * from items where itemNo in (select itemNo from orderItem where oId in (select orders.oId from orders inner join customer where orders.cNo = customer.cNo and cName="Mr. Patil"));
+select * from orders as o inner join customer as c on o.cNo = c.cNo and cName="Mr. Patil" inner join orderItem as oi on oi.oId=o.oId inner join items as i on i.itemNo=oi.itemNo;
 
-select * from warehouse where wId = (select wId from store group by wId having count(wId)=(select max(mycount) from (select count(wId)as mycount from store group by wId) as maxStore));
+select w.* from warehouse as w inner join store as s on w.wId=s.wId group by w.wId order by count(w.wId) desc limit 1;
 
-select * from items where itemNo = (select itemNo from orderItem group by itemNo having count(itemNo) = (select min(mycount) from (select count(itemNo) as mycount, itemNo from orderItem group by itemNo) as minTable));
-
-select orderItem.oId, orders.oDate, orderItem.quantity, customer.cNo, customer.cName, items.itemDescription from (((customer inner join orders on customer.cNo=orders.cNo)inner join orderItem on orders.oId = orderItem.oId) inner join items on orderItem.itemNo = items.itemNo) order by cNo;
+select i.* from orderItem as oi inner join items as i on i.itemNo=oi.itemNo group by oi.itemNo order by count(oi.itemNo) asc limit 1;
